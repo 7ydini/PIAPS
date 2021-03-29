b6e1e264bc7f8155784b09ba4f26e70cf6403684
7ydini <61342487+7ydini@users.noreply.github.com>
2021-03-29 17:08:46 +0300

    Update PeopleFactory.h

--- Builder/AbstractFactory/PeopleFactory.h
+++ Builder/AbstractFactory/PeopleFactory.h
@@ -10,10 +10,10 @@ public:
 	PeopleFactory() : p(0) {}
 	virtual ~PeopleFactory() {}
 	virtual void createBoard() {}
-	virtual void buildOldPas() {}
-	virtual void buildLgotPas() {}
-	virtual void buildChilPas() {}
-	virtual void buildDriver() {}
+	virtual void buildPassenger() {}
+	//virtual void buildLgotPas() {}
+	//virtual void buildChilPas() {}
+	//virtual void buildDriver() {}
 	//virtual void buildElephant() {}
 	virtual People* getPeople() { return p; }
 };
