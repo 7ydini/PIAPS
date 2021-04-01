8d1d9e5c0ac077de32ddb7e5f9e46592643300be
7ydini <61342487+7ydini@users.noreply.github.com>
2021-04-01 18:31:06 +0300

    Add files via upload

---
+++ BuilderPattern/BuilderPattern/AbstractFactory/PeopleFactory.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "People.h"
+
+class PeopleFactory
+{
+protected:
+	People* p;
+public:
+	PeopleFactory() : p(0) {}
+	virtual ~PeopleFactory() {}
+	virtual void createBoard() {}
+	virtual void buildOldPas() {}
+	virtual void buildLgotPas() {}
+	virtual void buildChilPas() {}
+	virtual void buildDriver() {}
+	//virtual void buildPassenger() {}
+
+	virtual People* getPeople() { return p; }
+};
+
